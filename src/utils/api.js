353063099ba1b15bdee9e0a4bd13bbcb0c125d53
Rohit Mondal <rohitmondal.22@nshm.edu.in>
2026-06-04 22:54:50 +0530

    update endpoint

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import safeStorage from './safeStorage';
 
 const api = axios.create({
-  baseURL:'http://localhost:5000/api',
+  baseURL:'https://api.kirtijob.com/api',
   timeout: 8000,
   headers: {
     'Content-Type': 'application/json'
