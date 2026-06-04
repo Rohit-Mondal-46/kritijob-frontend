60c70430d939638ad600d20e4f3384f518778811
Rohit Mondal <rohitmondal.22@nshm.edu.in>
2026-06-04 23:02:32 +0530

    fix:url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import safeStorage from './safeStorage';
 
+const apiBaseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+
 const api = axios.create({
-  baseURL:'https://api.kirtijob.com/api',
+  baseURL: apiBaseURL,
   timeout: 8000,
   headers: {
     'Content-Type': 'application/json'
